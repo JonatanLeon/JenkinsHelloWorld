767f75bdfc3f949abc9e49eb035e7e473d5d8dae
JonatanLeon <100357396+JonatanLeon@users.noreply.github.com>
2022-09-20 13:26:19 +0200

    Update Application.java

--- Application.java
+++ Application.java
@@ -1,11 +1,7 @@
-package app;
-
-import java.util.Date;
-
 public class Application {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome to Jenkins training");
+		System.out.println(" Made some changes");
 	}
 
 }
